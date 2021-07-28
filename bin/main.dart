097458fb0d41a352e21a08e249c3fc86327a4cd5
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-07-28 13:08:33 -0300

    Caracteres Simples

--- bin/main.dart
+++ bin/main.dart
@@ -1,13 +1,15 @@
 import 'aulas/c002a015.dart';
 import 'aulas/c003a016.dart';
 import 'aulas/c003a018.dart';
+import 'aulas/c003a019.dart';
 
 void main() {
 //
 
   //c002a015();
   //c003a016();
-  c003a018();
+  //c003a018();
+  c003a019();
 
 //
 }
