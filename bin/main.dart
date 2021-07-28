962d4bd817daac2f1714f22c8d522a072b3e79d7
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-07-28 15:31:04 -0300

    Exemplo de Split de String com Regex

--- bin/main.dart
+++ bin/main.dart
@@ -2,6 +2,8 @@ import 'aulas/c002a015.dart';
 import 'aulas/c003a016.dart';
 import 'aulas/c003a018.dart';
 import 'aulas/c003a019.dart';
+import 'aulas/c003a020.dart';
+import 'aulas/split_string_regex.dart';
 
 void main() {
 //
@@ -9,7 +11,10 @@ void main() {
   //c002a015();
   //c003a016();
   //c003a018();
-  c003a019();
+  //c003a019();
+  //c003a020();
+
+  spliStringRegex();
 
 //
 }
