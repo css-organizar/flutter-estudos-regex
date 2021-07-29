d9b5f727eef45b243d5e1d5fc72fb41e86cc0016
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-07-28 21:35:27 -0300

    Flags

---
+++ bin/aulas/c002a009.dart
@@ -0,0 +1,30 @@
+import '../commons/funcoes.dart';
+
+void c002a009() {
+//
+
+  print("\nCurso Regex Udemy - Capitulo 2 - Aula 09 \nFlags\n");
+
+  String texto;
+  RegExp regex;
+
+  /**
+   * A função "allMatches" Age como se fosse uma expressão com o flag "g - global" 
+   */
+
+  texto = "AaAaAa";
+  regex = RegExp("a");
+  var match1 = regex.allMatches(texto);
+  print(match1);
+
+  /**
+   * A função "allMatches" Age como se fosse uma expressão com o flag "g - global"
+   * O parâmetro "caseSensitive" age como se fosse o flaf "i - ignore case"
+   */
+  texto = "AaAaAa";
+  regex = RegExp("a", caseSensitive: false);
+  var match2 = regex.allMatches(texto);
+  print(match2);
+
+//
+}
