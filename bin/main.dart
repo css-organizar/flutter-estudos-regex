69f5d01a262c53b5986f24c622788b92a3c6b515
Claudney Sarti Sessa <claudneysartisessa@gmail.com>
2021-07-28 21:38:46 -0300

    Inclusão de novas aulas

--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,40 @@
+import 'aulas/c002a009.dart';
 import 'aulas/c002a015.dart';
 import 'aulas/c003a016.dart';
 import 'aulas/c003a018.dart';
 import 'aulas/c003a019.dart';
 import 'aulas/c003a020.dart';
+import 'aulas/c003a021.dart';
+import 'aulas/c003a022.dart';
+import 'aulas/c003a023.dart';
+import 'aulas/c003a024.dart';
+import 'aulas/c003a025.dart';
+import 'aulas/c003a026.dart';
+import 'aulas/c003a027.dart';
 import 'aulas/split_string_regex.dart';
 
 void main() {
 //
 
-  //c002a015();
-  //c003a016();
-  //c003a018();
-  //c003a019();
-  c003a020();
+  /**
+   * Remova o comentário da aula desejada e
+   * execute o código com F5 no Visual Studio Code
+   * para ver o resultado
+   */
+
+  // c002a009();
+  // c002a015();
+  // c003a016();
+  // c003a018();
+  // c003a019();
+  // c003a020();
+  // c003a021();
+  // c003a022();
+  // c003a023();
+  // c003a024();
+  // c003a025();
+  // c003a026();
+  c003a027();
 
   //spliStringRegex();
 
